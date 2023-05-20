385a208537f515e21f57e0375723136daf234c7a
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-20 12:31:42 +0530

    first commit

---
+++ LeetCode53.js
@@ -0,0 +1,21 @@
+function MaxSubArray(arr)
+{
+    let max = arr[0]
+    let sum = 0
+    for(let i = 0 ;i<arr.length ;i++){
+        sum = sum + arr[i]
+       
+        if(sum > max){
+            max = sum
+         
+        }
+        if(sum < 0){
+            sum = 0
+        }
+
+    }
+    return max;
+
+}
+
+console.log(MaxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
